28d341a2921c2ae00832377fbc76e211dc08da18
rnaisa <54948369+rnaisa@users.noreply.github.com>
2024-06-30 03:42:05 +0200

    deploy to sepolia testnet and verify

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,7 @@ const { vars } = require("hardhat/config");
 
 const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
 const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -13,5 +14,8 @@ module.exports = {
       url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [SEPOLIA_PRIVATE_KEY]
     }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
   }
 };
